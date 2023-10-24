8d7966a391f07343c45d1b17a13f37821c92b885
Владимир <kalenskiy.man@icloud.com>
2023-10-25 02:57:49 +0300

    Finished 6 HW

---
+++ task1.py
@@ -0,0 +1,28 @@
+"""Задание к 6 семинару"""
+class NumsLists:
+    """Класс, содержащий два списка"""
+    def __init__(self, lst1: list[int | float], lst2: list[int | float]):
+        self.lst1 = lst1
+        self.lst2 = lst2
+
+    def get_lists_averages(self) -> tuple[float, float]:
+        """Выявление средних значений списков"""
+        avg1 = 0
+        if self.lst1:
+            avg1 = sum(self.lst1) / len(self.lst1)
+
+        avg2 = 0
+        if self.lst2:
+            avg2 = sum(self.lst2) / len(self.lst2)
+
+        return avg1, avg2
+
+    def compare_averages(self) -> None:
+        """Сравнивает средние значения списков"""
+        avg1, avg2 = self.get_lists_averages()
+        if avg1 > avg2:
+            print('Первый список имеет большее среднее значение')
+        elif avg1 < avg2:
+            print('Второй список имеет большее среднее значение')
+        else:
+            print('Средние значения равны')
